91e2857faa7cbafef78d2c5d3dcc46071ffcd067
yaimadeddine <yo.aimadeddine@gmail.com>
2023-04-25 02:41:54 +0100

    add ajout voiture et update

--- ApplicationGestionLocationVoitures/LocationVoitures/views.py
+++ ApplicationGestionLocationVoitures/LocationVoitures/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Client, Voiture, Reservation
+from .Forms import VoitureForm
 
 
 # Admin Dashboard views
@@ -15,9 +16,28 @@ def client_list(request):
 
 def voiture_list(request):
     voitures = Voiture.objects.all()
-    return render(request, 'adminDashboard/voiture.html', {'voitures': voitures})
+    if request.method == 'POST':
+        form = VoitureForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('voiture-url')
+    else:
+        form = VoitureForm()
+    return render(request, 'adminDashboard/voiture.html', {'voitures': voitures, 'form': form})
 
 
 def reservation_list(request):
     reservations = Reservation.objects.all()
-    return render(request, 'adminDashboard/reservation.html', {'reservations': reservations})
+    context = {'reservations': reservations}
+    return render(request, 'adminDashboard/reservation.html', context)
+
+
+def create_car(request):
+    if request.method == 'POST':
+        form = VoitureForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('voiture_list')
+        else:
+            form = VoitureForm()
+        return render(request, 'adminDashboard/reservation.html', {'form': form})
